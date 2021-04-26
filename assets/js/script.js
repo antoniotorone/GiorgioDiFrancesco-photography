a0598d57d40b1668d00499ce4d4f99d5000004c8
antoniotorone <antoniotorone8913@gmail.com>
2021-04-26 22:02:31 +0000

    codes for get the price of packages added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,27 @@ $(document).ready(function(){
     $("button").click(function(){
        $("#about").toggle("medium")
     })
+
+
+    /*POST PRODUCTION*/
+    
+
+   
+    $("#basic").click(function(){
+        $("#price-1").toggle('medium').text("5$")
+    })
+
+     $("#silver").click(function(){
+        $("#price-2").toggle('medium').text("10$")
+    })
+
+     $("#gold").click(function(){
+        $("#price-3").toggle('medium').text("20$")
+    })
+
+
+
+
+    
     
 })
