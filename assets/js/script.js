ce1e2f4ecc7b004220e55c59ca281e6c857ccd39
antoniotorone <antoniotorone8913@gmail.com>
2021-04-27 19:35:33 +0000

    maint:packages buttons and price buttons codes added, style:packages buttons background-color added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,64 @@
-$(document).ready(function(){
-  
+$(document).ready(function () {
 
-    $("button").click(function(){
-       $("#about").toggle("medium")
+
+    $("button").click(function () {
+        $("#about").toggle("medium")
     })
 
 
     /*POST PRODUCTION*/
-    
 
-   
-    $("#basic").click(function(){
-        $("#price-1").toggle('medium').text("5$")
+    /*Packages*/
+
+    $("#copper").click(function () {
+        $("#package-1").toggle('medium').text("Photo retouching , Illustration and Coloring")
+        
     })
 
-     $("#silver").click(function(){
-        $("#price-2").toggle('medium').text("10$")
+    $("#copper").mouseenter(function(){
+        $(this).css("background-color" , "brown").mouseleave(function(){
+            $(this).css("background-color" , "black")
+        })
     })
 
-     $("#gold").click(function(){
-        $("#price-3").toggle('medium').text("20$")
+
+    $("#silver").click(function () {
+        $("#package-2").toggle('medium').text("Photo retouching , Illustration and Coloring , Photo Manipulation Editing")
+    })
+
+    $("#silver").mouseenter(function(){
+        $(this).css("background-color" , "silver").mouseleave(function(){
+            $(this).css("background-color" , "black")
+        })
+    })
+
+    $("#gold").click(function () {
+        $("#package-3").toggle('medium').text("Photo retouching , Illustration and Coloring , Photo Manipulation Editing , Artistic Effects")
+    })
+    
+    $("#gold").mouseenter(function(){
+        $(this).css("background-color" , "gold").mouseleave(function(){
+            $(this).css("background-color" , "black")
+        })
+    })
+
+    /*prices*/
+
+    $("#get-price-1").click(function(){
+        $("#price-1").toggle("medium").text("15£ per item")
+    })
+
+     $("#get-price-2").click(function(){
+        $("#price-2").toggle("medium").text("30£ per item")
+    })
+
+     $("#get-price-3").click(function(){
+        $("#price-3").toggle("medium").text("45£ per item")
     })
 
 
 
 
-    
-    
+
+
 })
