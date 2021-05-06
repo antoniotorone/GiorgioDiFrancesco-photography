8de62eafde82c77c28a0cab0e12769ed3ea005d8
antoniotorone <antoniotorone8913@gmail.com>
2021-05-06 17:43:09 +0000

    sendMail function included that call the form in contact.html

---
+++ assets/js/emailjs.js
@@ -0,0 +1,20 @@
+function sendMail(contactForm){
+        emailjs.send("service_jee9pg2","Giorgio photography" ,{
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "service_request": contactForm.messagerequest.value
+        })
+
+        .then(
+            function(response){
+                console.log("SUCCESS" , response);
+            },
+            function(error){
+                console.log("FAILED" , error);
+            }
+
+        );
+        return false;
+
+        
+    }
