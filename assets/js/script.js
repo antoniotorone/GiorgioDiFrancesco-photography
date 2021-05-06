767ed9779fe04cc264f5f62ccd910a2a4bce0748
antoniotorone <antoniotorone8913@gmail.com>
2021-05-06 17:34:11 +0000

    the map and emailjs codes has been removed and restored in seperate files

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     /*INDEX.HTML*/
 
     $("button").click(function () {
-        $("#about").toggle("medium")
-    })
+        $("#about").toggle("medium");
+    });
 
 
     /*POST PRODUCTION.HTML*/
@@ -13,58 +13,51 @@ $(document).ready(function () {
     /*Packages*/
 
     $("#copper").click(function () {
-        $("#package-1").toggle('medium').text("Photo retouching , Illustration and Coloring")
-        
-    })
+        $("#package-1").toggle('medium').text("Photo retouching , Illustration and Coloring");
 
-    $("#copper").mouseenter(function(){
-        $(this).css("background-color" , "brown").mouseleave(function(){
-            $(this).css("background-color" , "black")
-        })
-    })
+    });
+
+    $("#copper").mouseenter(function () {
+        $(this).css("background-color", "brown").mouseleave(function () {
+            $(this).css("background-color", "black");
+        });
+    });
 
 
     $("#silver").click(function () {
-        $("#package-2").toggle('medium').text("Photo retouching , Illustration and Coloring , Photo Manipulation Editing")
-    })
+        $("#package-2").toggle('medium').text("Photo retouching , Illustration and Coloring , Photo Manipulation Editing");
+    });
 
-    $("#silver").mouseenter(function(){
-        $(this).css("background-color" , "silver").mouseleave(function(){
-            $(this).css("background-color" , "black")
-        })
-    })
+    $("#silver").mouseenter(function () {
+        $(this).css("background-color", "silver").mouseleave(function () {
+            $(this).css("background-color", "black");
+        });
+    });
 
     $("#gold").click(function () {
-        $("#package-3").toggle('medium').text("Photo retouching , Illustration and Coloring , Photo Manipulation Editing , Artistic Effects")
-    })
-    
-    $("#gold").mouseenter(function(){
-        $(this).css("background-color" , "gold").mouseleave(function(){
-            $(this).css("background-color" , "black")
-        })
-    })
+        $("#package-3").toggle('medium').text("Photo retouching , Illustration and Coloring , Photo Manipulation Editing , Artistic Effects");
+    });
+
+    $("#gold").mouseenter(function () {
+        $(this).css("background-color", "gold").mouseleave(function () {
+            $(this).css("background-color", "black");
+        });
+    });
 
     /*prices*/
 
-    $("#get-price-1").click(function(){
-        $("#price-1").toggle("medium").text("15£ per item")
-    })
+    $("#get-price-1").click(function () {
+        $("#price-1").toggle("medium").text("15£ per item");
+    });
 
-     $("#get-price-2").click(function(){
-        $("#price-2").toggle("medium").text("30£ per item")
-    })
+    $("#get-price-2").click(function () {
+        $("#price-2").toggle("medium").text("30£ per item");
+    });
 
-     $("#get-price-3").click(function(){
-        $("#price-3").toggle("medium").text("45£ per item")
-    })
-
-
-    /*MY JOURNEY.HTML*/
-
-  
-    
+    $("#get-price-3").click(function () {
+        $("#price-3").toggle("medium").text("45£ per item");
+    });
 
 
     
-
-})
+});
