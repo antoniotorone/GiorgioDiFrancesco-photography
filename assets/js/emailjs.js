92149f053eee2c968d6eb92f03e5fab1121836ee
antoniotorone <antoniotorone8913@gmail.com>
2021-05-10 23:10:40 +0000

    messages added fo success and failed function to display after the submit of the form

--- assets/js/emailjs.js
+++ assets/js/emailjs.js
@@ -7,17 +7,20 @@ function sendMail(contactForm){
 
         .then(
             function(response){
-                console.log("SUCCESS" , response);
+                console.log("SUCCESS",response)
+                $(".test").text("Thanks for getting in contact your request is successful");
             },
             function(error){
-                console.log("FAILED" , error);
+                console.log("FAILED",error)
+                $(".test").text("Sorry your request is failed. Please try again later ");
             }
+
+
+
 
 
         );
         return false;
 
-        
+}
 
-        
-    }
