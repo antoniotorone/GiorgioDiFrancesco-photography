51bbf6fe47b626495b09b9e70dca2806c378010d
antoniotorone <antoniotorone8913@gmail.com>
2021-05-11 18:23:10 +0000

    maint:rest method to reset the form after sumnit added, tested, work perfect.Success message modified

--- assets/js/emailjs.js
+++ assets/js/emailjs.js
@@ -1,26 +1,25 @@
-function sendMail(contactForm){
-        emailjs.send("service_jee9pg2","Giorgio photography" ,{
+function sendMail(contactForm) {
+    emailjs.send("service_jee9pg2", "Giorgio photography", {
             "from_name": contactForm.name.value,
             "from_email": contactForm.emailaddress.value,
             "service_request": contactForm.messagerequest.value
         })
 
         .then(
-            function(response){
-                console.log("SUCCESS",response)
-                $(".test").text("Thanks for getting in contact your request is successful");
+            function (response) {
+                console.log("SUCCESS", response)
+                alert("Thanks for getting in contact your request is successfully sent");
             },
-            function(error){
-                console.log("FAILED",error)
-                $(".test").text("Sorry your request is failed. Please try again later ");
+            function (error) {
+                console.log("FAILED", error)
+                alert("Sorry your request is failed. Please try again later ");
             }
 
-
-
-
-
         );
-        return false;
+
+
+        contactForm.reset()
+
+return false;
 
 }
-
